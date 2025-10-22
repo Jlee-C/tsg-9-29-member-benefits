b13bb9e8d02c63f26eb8d71173043b07b3c30051
Josh Lee <jleec@yorksolutions.net>
2025-10-22 12:40:59 -0500

    added SecurityConfig and security filterchain

---
+++ backend/src/main/java/com/yorksolutions/backend/config/SecurityConfig.java
@@ -0,0 +1,42 @@
+package com.yorksolutions.backend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable) // So I don't have to mess with csrf tokens for the mean time
+                .cors(cors -> {})
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/public/**", "/login/**", "/error").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .oauth2Login(oauth2 -> oauth2
+                        .successHandler(successHandler()) // successHandler for routing to dashboard
+                )
+                .logout(logout -> logout
+                        .logoutUrl("/api/auth/logout")
+                        .deleteCookies("JSESSIONID")
+                        .logoutSuccessHandler((req, resp, auth) -> resp.setStatus(200))
+                );
+
+        return http.build();
+    }
+
+    // Redirects to React dashboard
+    @Bean
+    public AuthenticationSuccessHandler successHandler() {
+        return (request, response, authentication) -> {
+            response.sendRedirect("http://localhost:5137/dashboard");
+        };
+    }
+
+}
