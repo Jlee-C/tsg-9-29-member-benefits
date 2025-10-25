db3efc8350d651a974ff292aec1eba05e4504c2d
Josh Lee <jleec@yorksolutions.net>
2025-10-24 22:00:08 -0500

    updated cors host port -> 5173

--- backend/src/main/java/com/yorksolutions/backend/config/CorsConfig.java
+++ backend/src/main/java/com/yorksolutions/backend/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5137"));
+        config.setAllowedOrigins(List.of("http://localhost:5173"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(false);
